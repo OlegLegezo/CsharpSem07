91ed945d0b6636ecdb9af35778d34e71c23c6911
OlegLegezo <oleglegezo@yandex.ru>
2022-06-10 17:27:14 +0300

    поиск чётных элементов двумерного массива

---
+++ ex49/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] GetArray(int a, int b) //метод заполнения массива целыми числами
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-строки
+        {
+            array[i, j] = rnd.Next(0, 10); //или array[i, j]=rnd.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array) //метод печати массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-строки
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arrayResult = GetArray(5, 7);
+PrintArray(arrayResult);
+
+int[,] ChetnyElemSumm(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки можно экономить память пройтись по четным только i+=2
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-столбцы можно экономить память пройтись по четным только j+=2
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+Console.WriteLine();
+int[,] arrayResult2 = ChetnyElemSumm(arrayResult);
+PrintArray(arrayResult2);
