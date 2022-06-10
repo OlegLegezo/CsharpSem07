2423c0781c51121d669252768c28241ba8872197
OlegLegezo <oleglegezo@yandex.ru>
2022-06-10 16:38:43 +0300

    метод заполнения массива целыми числами, метод печати массива

---
+++ ex46/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] GetArray(int a, int b) //метод заполнения массива целыми числами
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-строки
+        {
+            array[i, j]=rnd.Next(0,10); //или array[i, j]=rnd.Next(10);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] array) //метод печати массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-строки
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,]arrayResult=GetArray(5,7);
+PrintArray(arrayResult);
